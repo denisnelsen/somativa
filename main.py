693c6c9e3abb7dcbbc1e7d19ccf8150364455785
denisnelsen <denisnelsen@gmail.com>
2025-04-15 18:18:13 -0300

    Adicionando testes no docker

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import random
-
 from fastapi import FastAPI
+import random
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+
+class Estudante(BaseModel):
+    name: str
+    curso: str
+    ativo: bool
+
+
+    
 
 @app.get("/helloword")
 async def root():
@@ -11,4 +20,17 @@ async def root():
 
 @app.get("/funcaoteste")
 async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
+    return {"teste": True, "num_aleatorio": random.randint(0, 20000)}
+
+
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+@app.put("/estudantes/update/{id_estudante}")
+async def update_item(id_estudante: int):
+    return id_estudante > 0
+
+@app.delete("/estudantes/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
